bcd52f98598c250792c985dd65c10f1d5e6243ce
Elliot67 <elliot.lintz@gmail.com>
2020-03-15 17:44:15 +0100

    [ADD] Dynamisation js section membres & corrections historic

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ import { Header } from "@scripts/header";
 import { Slider } from "@scripts/slider";
 import { LazyLoader } from "@scripts/lazyLoader";
 import { ScrollAnimation } from "@scripts/scrollAnimation";
+import { Members } from "@scripts/members";
 import { Form } from "@scripts/form";
 
 new Header();
 new Slider();
+new Members();
 new Form();
 
 document.addEventListener("DOMContentLoaded", () => {
---
+++ src/scripts/members.js
@@ -0,0 +1,57 @@
+export class Members {
+	constructor() {
+		this.elements = [].slice.call(document.querySelectorAll('.Members-content'));
+		this.heads = document.querySelectorAll('.Members-head');
+		this.currentTab = document.querySelector('.Members .activeTab');
+		this.currentHead = document.querySelector('.Members .activeHead');
+
+		console.log(this.currentTab);
+
+		console.log(this.currentHead);
+		this.isRunning = false;
+
+		this.events();
+	}
+
+	events() {
+		for (const head of this.heads) {
+			head.addEventListener('click', (event) => {
+				console.log(this.isRunning);
+				if(!this.isRunning) {
+					console.log('je rentre');
+					this.isRunning = true;
+
+					const target = document.querySelector(".Members-content[data-name='" + event.target.dataset.name + "']");
+					if (this.currentTab !== target) {
+						this.animate(this.currentTab, target)
+						.finally(() => {
+							this.currentHead.classList.remove('activeHead');
+							event.target.classList.add('activeHead');
+							this.currentHead = event.target;
+							this.currentTab = target;
+							window.setTimeout(() => {
+								this.isRunning = false;
+							}, 500);
+						});
+					} else {
+						this.isRunning = false;
+					}
+				}
+			});
+		}
+	}
+
+	async animate(leave, enter) {
+		new Promise(function(resolve, reject) {
+			leave.classList.remove('activeTab');
+			window.setTimeout(() => {
+				leave.classList.add('hideTab');
+				enter.classList.remove('hideTab');
+				window.setTimeout(() => {
+					enter.classList.add('activeTab');
+					resolve();
+				}, 50);
+			}, 500);
+		});
+	}
+}
